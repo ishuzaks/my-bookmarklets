7e801f8033dc616c72538e153fcd09a2831391db
ishuzaks <k.suzuki512+devlop@gmail.com>
2024-10-05 21:29:27 +0900

    TypeScript導入

---
+++ src/DLsiteVoiceWorkInfoExporter.ts
@@ -0,0 +1,77 @@
+function main(): void {
+  const workNameElement = document.getElementById("work_name");
+
+  if (workNameElement === null || workNameElement.textContent === null) {
+    alert("作品名を取得できませんでした。");
+    return;
+  }
+  const workName = workNameElement.textContent.trim();
+
+  const makerNameElement = document.querySelector("span.maker_name");
+  if (makerNameElement === null || makerNameElement.textContent === null) {
+    alert("サークル名を取得できませんでした。");
+    return;
+  }
+  const makerName = makerNameElement.textContent.trim();
+  const saveFileName = ("[" + makerName + "] " + workName).replace(
+    /[:*?"<>|/\\]/g,
+    (e: string): string => {
+      const map = new Map<string, string>([
+        [":", "："],
+        ["*", "＊"],
+        ["!", "！"],
+        ["?", "？"],
+        ['"', "”"],
+        ["<", "＜"],
+        [">", "＞"],
+        ["|", "｜"],
+        ["/", "／"],
+        ["\\", "￥"],
+      ]);
+      return map.get(e) ?? e;
+    }
+  );
+  const yearMatchedObj = document
+    .querySelector('a[href*="year"]')
+    ?.getAttribute("href")
+    ?.match(/\/year\/(\d+)\//);
+  const year = yearMatchedObj ? yearMatchedObj[1] : "";
+  const voiceActorXPathResult = document.evaluate(
+    "//th[contains(text(), '声優')]/following-sibling::td/a",
+    document,
+    null,
+    XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+    null
+  );
+  const voiceActors: string[] = [];
+  for (let i = 0; i < voiceActorXPathResult.snapshotLength; i++) {
+    const voiceActorElement = voiceActorXPathResult.snapshotItem(i);
+    if (voiceActorElement !== null && voiceActorElement.textContent !== null) {
+      voiceActors.push(voiceActorElement.textContent.trim());
+    }
+  }
+  const voiceActorsStr = voiceActors.join(", ");
+  const blob = new Blob(
+      [
+        voiceActorsStr +
+          " " +
+          workName +
+          " " +
+          year +
+          " HVoiceDrama " +
+          makerName,
+      ],
+      {
+        type: "text/plain",
+      }
+    ),
+    downloadURL = URL.createObjectURL(blob),
+    downloadLink = document.createElement("a");
+  downloadLink.href = downloadURL;
+  downloadLink.download = `${saveFileName}.txt`;
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+  URL.revokeObjectURL(downloadURL);
+}
+main();
